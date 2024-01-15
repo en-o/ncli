c800577f7af72b5a806177d5414282a8f56583ae
en-o <13320385140@163.com>
2024-01-15 16:29:20 +0800

    cli demo 注释

--- README.md
+++ README.md
@@ -5,8 +5,23 @@ rust 构建的命令行工具
 
 
 # 依赖
-## [命令行库](https://docs.rs/clap/latest/clap/)
+## [命令行库 clap](https://docs.rs/clap/latest/clap/)
+> 使用 #[] 语法定义的属性注解  
 
+### [属性分类](https://docs.rs/clap/4.2.1/clap/_derive/index.html#terminology)
+#### Raw attributes
+> 
+- derive
+- command [建一个命令行界面]
+- arg [命令行参数的抽象表示]
+
+
+#### Magic attributes
+>  控制每一个具体的功能项目
+- author
+- version
+- long
+- short
 
 # demo 
 > windows 
--- src/demo.rs
+++ src/demo.rs
@@ -1,16 +1,22 @@
 use clap::Parser;
 
+// clap ： https://www.cnblogs.com/SleepSupervisor/p/17757751.html
+
+
 /// Simple program to greet a person
-/// 定义接受参数
+/// 字段就是命令行的参数名称
+///
 #[derive(Parser, Debug)]
+// 制命令行展示的行为
 #[command(author, version, about, long_about = None)]
 pub struct Args {
     /// Name of the person to greet
+    /// arg 控制单个参数的信息
     #[arg(short, long)]
     pub name: String,
 
     /// Number of times to greet
-    /// 循环次数
+    /// arg 控制单个参数的信息
     #[arg(short, long, default_value_t = 1)]
     pub count: u8,
 }
