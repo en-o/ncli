ab0b0deaf5ece69ece3f1709892fc0d8562984e2
en-o <13320385140@163.com>
2024-01-15 15:49:13 +0800

    cli demo 修改目录结构

--- src/demo.rs
+++ src/demo.rs
@@ -12,3 +12,11 @@ pub struct Args {
     #[arg(short, long, default_value_t = 1)]
     pub count: u8,
 }
+
+
+pub fn test(){
+    let args = Args::parse();
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use clap::Parser;
-use crate::demo::Args;
 
 mod demo;
 
 fn main() {
-    let args = Args::parse();
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
-    }
+    demo::test();
 }
