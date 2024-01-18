3383dd77dbdf854480b4c344b09e4463dcfa2187
en-o <13320385140@163.com>
2024-01-18 10:44:51 +0800

    /// 统一打印

--- src/system/pid.rs
+++ src/system/pid.rs
@@ -10,19 +10,27 @@ pub(crate) fn dispose_pid(port: &Option<u16>) {
             let mut system = System::new_all();
             // 刷新系统信息
             system.refresh_all();
-            for (pid, process) in  system.processes() {
-                println!("IP(\x1b[32;1m{}\x1b[0m) || 应用({}) || 状态({})  || 路径({:?})",
-                         pid,
-                         process.name(),
-                         process.status(),
-                         process.cwd().map(|p|p.to_str()));
+            for (pid, process) in system.processes() {
+                print(pid.as_u32(),
+                      process.name(),
+                      &process.status().to_string(),
+                      process.cwd().map(|p| p.to_str()).unwrap_or_default()
+                );
             }
         }
         Some(assign) => {
             println!("assign: {assign:?}")
         }
     }
-
 }
 
+
+/// 统一打印
+fn print(pid: u32, name: &str, status: &String, path: Option<&str>) {
+    println!("IP(\x1b[32;1m{}\x1b[0m) || 应用({}) || 状态({})  || 路径({:?})",
+             pid,
+             name,
+             status,
+             path);
+}
 
