9649944465f5f5e97dbdde5dbd4dd3dc772a120b
en-o <13320385140@163.com>
2024-02-04 15:12:49 +0800

    新增注释

--- src/nginx/tcp_html.rs
+++ src/nginx/tcp_html.rs
@@ -14,15 +14,18 @@ pub(crate) async fn dispose_html(path: &String, port: &u16, prefix: &Option<Stri
     let serve_dir
         = ServeDir::new(path);
 
-    let mut prefix_str = "/";
+
     // 构建前缀
+    let mut prefix_str = "/";
     match prefix {
         Some(pr) => {
             prefix_str = pr;
         }
         _ => {}
     }
+
     let app = Router::new()
+        // 将  `prefix_str`  URL 映射到  serve_dir 目录下的哪些文件
         .nest_service(prefix_str, serve_dir.clone());
     // 创建 tcp 连接
     let addr = format!("0.0.0.0:{}",port);
